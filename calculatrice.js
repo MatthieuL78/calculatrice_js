63f1406437980dce31fb84212626dfd767ed513e
MatthieuL78 <matthieu.lu@outlook.fr>
2018-05-22 19:14:59 +0200

    calcule yeah

--- calculatrice.js
+++ calculatrice.js
@@ -1,48 +1,42 @@
 
 $(document).ready(function(e) {
+	// Sélections des chiffres
     $('#1,#2,#3,#4,#5,#6,#7,#8,#9,#0').click(function(){
+    	$('#debbug').val("Boite d'erreur !");
 		var v = $(this).val();
 		$('#answer').val($('#answer').val() + v);	
 	});
+	// Pour nettoyer les valeurs et historique
 	$('#C').click(function(){
 		$('#answer').val('');
 		$('#operation').val('');
-		$('#operation').removeClass('activeAnswer');
+		$('#historique').text('');
+		$('#last_value').val('');
 		$('#equals').attr('onclick','');
 	});
+	// Les opérations
 	$('#plus').click(function(e) { 
 		if($('#answer').val() == ''){
+			$('#debbug').val('Selectionner un chiffre !');
 			// Si la case est vide (aucun chiffre n a etait ajoute, on ne prend rien en compte)
 			return false;
-			console.log($('#equals').attr('onclick','').val());
-			// $('#equals').attr('onclick','');
-		}
-		else if ( $('#operation').attr('class') == 'activeAnswer') {
-			$('#operation').val( $('#operation').val() + $('#plus').val() );
-			$('#answer').val('');
-			console.log($('#equals').attr('onclick','').val());
 			$('#equals').attr('onclick','');
 		}
 		else{
 			// Si la case possede un ou des numeros :
 			// On rajoute a la valeur de l'objet #operation le contenu du chiffre enregistree dans 
-			// #answer et on y ajoute la valeur de l'operateur (ici '+') 
+			// $('#answer').val => Valeur $('#plus').val => +
 			$('#operation').val( $('#operation').val() + $('#answer').val() + $('#plus').val() );
 			// On clear le contenu et on attend le prochain chiffre
 			$('#answer').val('');
-			console.log($('#equals').attr('onclick','').val());
 			$('#equals').attr('onclick','');
 		}
     });
 	$('#subtract').click(function(e) { 
 	
 		if($('#answer').val() == ''){
+			$('#debbug').val('Selectionner un chiffre !');
 			return false;
-			$('#equals').attr('onclick','');
-		}
-		else if ( $('#operation').attr('class') == 'activeAnswer') {
-			$('#operation').val( $('#operation').val() + $('#subtract').val() );
-			$('#answer').val('');
 			$('#equals').attr('onclick','');
 		}
 		else{
@@ -54,12 +48,8 @@ $(document).ready(function(e) {
 	$('#divide').click(function(e) { 
 	
 		if($('#answer').val() == ''){
+			$('#debbug').val('Selectionner un chiffre !');
 			return false;
-			$('#equals').attr('onclick','');
-		}
-		else if ( $('#operation').attr('class') == 'activeAnswer') {
-			$('#operation').val( $('#operation').val() + $('#divide').val() );
-			$('#answer').val('');
 			$('#equals').attr('onclick','');
 		}
 		else{
@@ -71,17 +61,28 @@ $(document).ready(function(e) {
 	$('#product').click(function(e) { 
 	
 		if($('#answer').val() == ''){
+			$('#debbug').val('Selectionner un chiffre !');
 			return false;
-			$('#equals').attr('onclick','');
-		}
-		else if ( $('#operation').attr('class') == 'activeAnswer') {
-			$('#operation').val( $('#operation').val() + $('#product').val() );
-			$('#answer').val('');
 			$('#equals').attr('onclick','');
 		}
 		else{
 			$('#operation').val( $('#operation').val() + $('#answer').val() + $('#product').val() );
 			$('#answer').val('');
+			$('#equals').attr('onclick','');
+		}
+    });	
+    // Pour récupérer la derniere valeur
+    $('#ans').click(function(e) { 
+	
+		if($('#last_value').val() == ''){
+			$('#debbug').val("L'historique est vide !");
+			console.log("prout 1");
+			return false;
+			$('#equals').attr('onclick','');
+		}
+		else{
+			console.log("prout 2");
+			$('#answer').val($('#last_value').val());
 			$('#equals').attr('onclick','');
 		}
     });	
@@ -98,9 +99,15 @@ $(document).ready(function(e) {
 			// On fait le calcul a l'aide du string
 			$('#answer').val(eval(c));
 			$('#operation').val(eval(c));
-			$('#operation').addClass('activeAnswer');
+			// On crée l historique
+			$('#last_value').val(eval(c));
+			console.log($('#historique').text());
+			var my_histo = $('#historique').text($('#historique').text() + '\n' + c + ' = ' + eval(c));
+			my_histo.html(my_histo.html().replace(/\n/g, '<br/>'));
+			// On vide les contenus
+			$('#answer').val('');
+			$('#operation').val('');
 			$('#equals').attr('onclick','return false');
-		
 		}
 	});
 });
